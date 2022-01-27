05a3db6d7114e227c6c00dccf08a42471e02f2fd
liucong <liucong@medlinker.com>
2022-01-27 18:50:50 +0800

    add service register

--- rpc/server.go
+++ rpc/server.go
@@ -1,16 +1,24 @@
 package rpc
 
+import "reflect"
+
 type Server interface {
 	// Register all exported method of service
-	Register(srv Service) error
+	Register(srv interface{})
 	Run() error
 }
 
 type server struct {
+	srvMap map[string]interface{}
 }
 
-func (s *server) Register(srv Service) error {
-	return nil
+func (s *server) Register(srv interface{}) {
+	ty := reflect.TypeOf(srv)
+	name := ty.Name()
+	if _, ok := s.srvMap[name]; ok {
+		panic("服务" + name + "重复注册")
+	}
+	s.srvMap[name] = srv
 }
 
 func (s *server) Run() error {
@@ -18,5 +26,7 @@ func (s *server) Run() error {
 }
 
 func NewServer() Server {
-	return &server{}
+	return &server{
+		srvMap: make(map[string]interface{}),
+	}
 }
