b6223aca755708ca9c5e450860deca68847344be
liucong <liucong@medlinker.com>
2022-01-29 14:09:36 +0800

    add gob

--- rpc/client.go
+++ rpc/client.go
@@ -11,27 +11,27 @@ import (
 type Client interface {
 	// Call , invoke service method ,like srv.method
 	Call(object interface{}, method string, args interface{}) (reply interface{}, err error)
+	Close() error
 }
 
 type client struct {
 	addr         string
 	codecFactory CodecFactory
 	codecType    int64
+	conn         net.Conn
+}
+
+func (c *client) Close() error {
+	return c.conn.Close()
 }
 
 func (c *client) Call(object interface{}, method string, args interface{}) (reply interface{}, err error) {
-	fmt.Print("准备连接")
-	conn, err := net.Dial("tcp", c.addr)
-	if err != nil {
-		return
-	}
-
 	fmt.Println("准备发起请求数据,codeType:", c.codecType)
-	json.NewEncoder(conn).Encode(&Option{CodeType: 1})
+	json.NewEncoder(c.conn).Encode(&Option{CodeType: c.codecType})
 
 	target := reflect.TypeOf(object).String()
 	// 写入头部
-	encoder := c.codecFactory.GetCodec(conn)
+	encoder := c.codecFactory.GetCodec(c.conn)
 	header := &Head{
 		ServiceMethod: target + "/" + method,
 		Seq:           "1",
@@ -42,7 +42,7 @@ func (c *client) Call(object interface{}, method string, args interface{}) (repl
 	encoder.WriteBody(args)
 
 	// 读取头部
-	decoder := json.NewDecoder(conn)
+	decoder := json.NewDecoder(c.conn)
 	decoder.Decode(header)
 	err = errors.New(header.Error)
 
@@ -52,5 +52,11 @@ func (c *client) Call(object interface{}, method string, args interface{}) (repl
 }
 
 func NewClient(addr string, codecType int64) Client {
-	return &client{addr: addr, codecFactory: NewCodecFactory(codecType), codecType: codecType}
+	fmt.Print("准备连接")
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Println("连接失败")
+	}
+	cli := &client{addr: addr, codecFactory: NewCodecFactory(codecType), codecType: codecType, conn: conn}
+	return cli
 }
