fdb2953ffd6e14e4c2b99221b43c70d8683b6045
liucong <liucong@medlinker.com>
2022-02-09 10:32:09 +0800

    finish rpc test

---
+++ rpc_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"next/rpc"
+	"testing"
+	"time"
+)
+
+type Test struct {
+}
+
+type Resp struct {
+	Name string
+}
+
+func (t *Test) HelloWorld(name string) (resp *Resp, err error) {
+	resp = &Resp{
+		Name: name,
+	}
+	return
+}
+
+func TestRpc(t *testing.T) {
+	test := new(Test)
+	server := rpc.NewServer("127.0.0.1:8396")
+	server.Register(test)
+	go server.Run()
+
+	time.Sleep(time.Second * 5)
+	client := rpc.NewClient("127.0.0.1:8396", 2)
+	defer client.Close()
+	resp, err := client.Call(test, "HelloWorld", "liucong")
+	fmt.Println(resp, err)
+}
